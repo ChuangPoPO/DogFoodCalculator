7cceeb10a3f4fcd41322211803470c38a301e586
ChuangPoPO <s842563@gmail.com>
2017-03-08 20:21:18 +0800

    取得今天的日期，下一步要將年月日切割出來。 today = new Date().toISOString().slice(0,10);
    將stage換算成日期和儒略日比較

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,41 @@ export class AppComponent {
   year:number = 2017;
   month:number = 1;
   day:number = 3;
-  //
+
+  today = new Date().toISOString().slice(0,10);
+  
+
   onClickSubmit():void {
-    console.log(this.kg);
-    console.log(this.year);
-    console.log(this.month);
-    console.log(this.day);
+    // console.log(this.kg);
+     console.log("year = " + this.year);
+     console.log("month = " + this.month);
+     console.log("day = " + this.day + "<br/>");
+
+     
+
+     console.log(this.today);
+     console.log(birthday);
+    //this.caculateSpoon(this.kg, this.birthday); //, this.today
+  }
+
+  private caculateSpoon(kg:number, birthday:Date): number { //,today:Date
+    let spoon:number = 0;
+    
+    let today:Date = new Date(); //https://angular.io/docs/ts/latest/guide/pipes.html
+
+    // let tDay:number = today.getDay();
+    // let tMonth:number = today.getMonth();
+    // let tYear:number = today.getFullYear();
+    
+    //let today: number = Date.now();
+    //let birthday: number = Date.parse(this.year + "-" + this.month + "-" + this.day);
+    //let d = today - birthday;
+
+    //console.log(tYear);
+    //console.log(birthday);
+    //console.log(d);
+
+    return spoon;
   }
 
 }
