cb7672cfea64ca5632af1b18d216cfe988a251cb
ChuangPoPO <s842563@gmail.com>
2017-03-09 21:46:41 +0800

    可以將today的年月日分開了，但是月和日的數字不對

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TheDate } from './arrays';
 
 @Component({
   selector: 'app-root',
@@ -13,39 +14,50 @@ export class AppComponent {
   year:number = 2017;
   month:number = 1;
   day:number = 3;
-
-  today = new Date().toISOString().slice(0,10);
   
+  //birthday:TheDate;
+  //today:TheDate;
+
+  //birthday{this.year, this.month, this.day};
+
+  today = new Date();//.getDate();
+  // todayYear = new Date().toISOString().slice(0,4).valueOf();//(0,10)
+  // todayMonth = new Date().toISOString().slice(5,7);
+  // todayDay = new Date().toISOString().slice(8,10);
 
   onClickSubmit():void {
     // console.log(this.kg);
-     console.log("year = " + this.year);
-     console.log("month = " + this.month);
-     console.log("day = " + this.day + "<br/>");
+     console.log("Byear = " + this.year);
+     console.log("Bmonth = " + this.month);
+     console.log("Bday = " + this.day);
 
+    //  console.log("todayYear : " + this.todayYear);
+    //  console.log("todayMonth : " + this.todayMonth);
+    //  console.log("todayDay : " + this.todayDay);
+
+     let bJDN:number = this.julianDayNumber(this.year, this.month, this.day);
+     let tJDN:number = this.julianDayNumber(this.today.getFullYear(), this.today.getMonth(), this.today.getDay());
      
-
-     console.log(this.today);
-     console.log(birthday);
+     console.log("todayYear : " + this.today.getFullYear());
+     console.log("todayMonth : " + this.today.getMonth());
+     console.log("todayDay : " + this.today.getDay());
+     
+     console.log("bJDN : " + bJDN);
+     console.log("tJDN : " + tJDN);
     //this.caculateSpoon(this.kg, this.birthday); //, this.today
   }
 
-  private caculateSpoon(kg:number, birthday:Date): number { //,today:Date
+  private julianDayNumber(year:number, month:number, day:number): number {
+    year += 8000;
+    if(month < 3){
+      year--;
+      month+=12;
+    } 
+    return (year*365) + (year/4) - (year/100) + (year/400) - 1200820 + (month*153+3)/5 - 92 + day - 1 ;
+  }
+
+  private caculateSpoon(kg:number): number { //,today:Date
     let spoon:number = 0;
-    
-    let today:Date = new Date(); //https://angular.io/docs/ts/latest/guide/pipes.html
-
-    // let tDay:number = today.getDay();
-    // let tMonth:number = today.getMonth();
-    // let tYear:number = today.getFullYear();
-    
-    //let today: number = Date.now();
-    //let birthday: number = Date.parse(this.year + "-" + this.month + "-" + this.day);
-    //let d = today - birthday;
-
-    //console.log(tYear);
-    //console.log(birthday);
-    //console.log(d);
 
     return spoon;
   }
