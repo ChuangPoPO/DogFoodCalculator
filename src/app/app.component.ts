72b2b11e5b62bd33e8f3a21257301a5b993aeae0
ChuangPoPO <s842563@gmail.com>
2017-03-10 21:21:15 +0800

    計算出s年齡Stage階段

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TheDate } from './arrays';
 
 @Component({
   selector: 'app-root',
@@ -15,35 +14,21 @@ export class AppComponent {
   month:number = 1;
   day:number = 3;
   
-  //birthday:TheDate;
-  //today:TheDate;
+  //birthday:TheDate = new TheDate();
 
-  //birthday{this.year, this.month, this.day};
-
-  today = new Date();//.getDate();
+  today = new Date();
   // todayYear = new Date().toISOString().slice(0,4).valueOf();//(0,10)
-  // todayMonth = new Date().toISOString().slice(5,7);
-  // todayDay = new Date().toISOString().slice(8,10);
+ 
 
   onClickSubmit():void {
-    // console.log(this.kg);
-     console.log("Byear = " + this.year);
-     console.log("Bmonth = " + this.month);
-     console.log("Bday = " + this.day);
-
-    //  console.log("todayYear : " + this.todayYear);
-    //  console.log("todayMonth : " + this.todayMonth);
-    //  console.log("todayDay : " + this.todayDay);
-
+    // console.log(this.kg);    
      let bJDN:number = this.julianDayNumber(this.year, this.month, this.day);
      let tJDN:number = this.julianDayNumber(this.today.getFullYear(), (this.today.getMonth()+1) , this.today.getDate());
-     
-     console.log("todayYear : " + this.today.getFullYear());
-     console.log("todayMonth : " + (this.today.getMonth()+1));
-     console.log("todayDay : " + this.today.getDate());
-     
+     let stage:string = this.whichStage(bJDN, tJDN);
+
      console.log("bJDN : " + bJDN);
      console.log("tJDN : " + tJDN);
+     console.log("Stage : " + stage);
     //this.caculateSpoon(this.kg, this.birthday); //, this.today
   }
 
@@ -56,6 +41,19 @@ export class AppComponent {
     return (year*365) + (year/4) - (year/100) + (year/400) - 1200820 + (month*153+3)/5 - 92 + day - 1 ;
   }
 
+  private whichStage(bJDN:number, tJDN:number):string{
+    let difDays:number = tJDN - bJDN;
+    
+    if(difDays>=0 && difDays < 123){
+      return "stage1";
+    }else if(difDays >=123 && difDays <245){
+      return "stage2";
+    }else if(difDays >=245 && difDays <366){
+      return "stage3";
+    }else{
+      return "stage4";
+    }
+  }
   private caculateSpoon(kg:number): number { //,today:Date
     let spoon:number = 0;
 
